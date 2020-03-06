d6c5874a4b04e898788982d6655e393deb977959
sodreamon <60939443+sodreamon@users.noreply.github.com>
2020-03-06 22:54:01 +0900

    reservation
    admin list_display
    models in_progress
    " is_finished

--- rooms/models.py
+++ rooms/models.py
@@ -93,6 +93,7 @@ class Room(core_models.TimeStapedModel):
 
     def total_rating(self):
         all_reviews = self.reviews.all()
-        all_ratings = []
+        all_ratings = 0
         for review in all_reviews:
-            all_ratings.append(review.rating_average())
+            all_ratings += review.rating_average()
+        return all_ratings / len(all_reviews)
